0dbb8d1b3c7645580a411f949318cf2c65283483
Ashish <aashish7322@gmail.com>
2022-09-22 13:52:55 +0530

    MaxOf2NumUsing CompareTo modified

--- src/Day13PracticeProblems/CompareToInteger.java
+++ src/Day13PracticeProblems/CompareToInteger.java
@@ -9,15 +9,14 @@ public class CompareToInteger {
 		Integer num2 = Integer.valueOf(29);
 		Integer num3 = Integer.valueOf(11);
 
-		
 		int max = num1; // assume num1 is initially the largest
 
-	    if (num2.compareTo(max) > 0)
-	      max = num2; // num2 is the largest so far
+		if (num2.compareTo(max) > 0)
+			max = num2; // num2 is the largest so far
 
-	    if (num3.compareTo(max) > 0)
-	      max = num3; // num3 is the largest
+		if (num3.compareTo(max) > 0)
+			max = num3; // num3 is the largest
 
-	 System.out.println(max+" is the maximum of all");
+		System.out.println(max + " is the maximum of all");
 	}
 }
