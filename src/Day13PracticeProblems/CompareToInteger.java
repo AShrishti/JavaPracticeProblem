6ad07c8ad7632d8d85a341d52652414c0ea45f30
Ashish <aashish7322@gmail.com>
2022-09-22 16:16:51 +0530

    Compare 2 Strings

--- src/Day13PracticeProblems/CompareToInteger.java
+++ src/Day13PracticeProblems/CompareToInteger.java
@@ -2,13 +2,12 @@ package Day13PracticeProblems;
 
 public class CompareToInteger {
 
-	
-	public static void main(String[] args) {
+	public static int maximum(int a, int b, int c) {
 
-		Integer num1 = Integer.valueOf(10);
-		Integer num2 = Integer.valueOf(29);
-		Integer num3 = Integer.valueOf(11);
-
+		Integer num1 = Integer.valueOf(a);
+		Integer num2 = Integer.valueOf(b);
+		Integer num3 = Integer.valueOf(c);
+		// Integer num4 = new Integer(19);
 		int max = num1; // assume num1 is initially the largest
 
 		if (num2.compareTo(max) > 0)
@@ -16,7 +15,27 @@ public class CompareToInteger {
 
 		if (num3.compareTo(max) > 0)
 			max = num3; // num3 is the largest
+		return max;
+	}
 
-		System.out.println(max + " is the maximum of all");
+	public static String maximum(String a, String b, String c) {
+
+		String max = a; // assume a is initially the largest
+
+		if (b.compareToIgnoreCase(max) > 0)
+			max = b; // b is the largest so far
+
+		if (c.compareToIgnoreCase(max) > 0)
+			max = c; //  is the largest
+
+		return max;
+	}
+
+	public static void main(String[] args) {
+
+		int maxInt = CompareToInteger.maximum(10, 29, 39);
+		String maxString = CompareToInteger.maximum("apple", "Peach", "Banana");
+		System.out.println(maxInt + " is the maximum of all Integers");
+		System.out.println(maxString + " is the maximum of all input String");
 	}
 }
